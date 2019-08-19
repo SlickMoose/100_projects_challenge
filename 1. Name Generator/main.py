e91718aec2a0d780e19961240ad32d2e0b94d781
mrdominikku <dominik.ruczajewski@gmail.com>
2019-08-19 19:05:58 +0200

    added custom styles to widgets

--- 1. Name Generator/main.py
+++ 1. Name Generator/main.py
@@ -1,13 +1,24 @@
 import math
 import tkinter as tk
 from config import config_names
+from tkinter import ttk
 
 
-class NameGenerator:
+class NameGenerator(ttk.Frame):
 
-    def __init__(self, master):
+    def __init__(self, master=None):
 
+        ttk.Frame.__init__(self, master)
+
+        self.style = ""
+        self.configure_style()
         self.master = master
+
+        self.master.geometry("500x500")
+        self.master.resizable(0, 0)
+        self.master.title("Name Generator")
+        self.master.configure(background='#19232D')
+
         self.name_cat = self.set_categories()
         self.set_frame()
 
@@ -20,52 +31,74 @@ class NameGenerator:
     def set_frame(self):
 
         btn_width = 30
-        row_padding = 50
 
-        mainframe = tk.Frame(master=self.master)
-        mainframe.grid()
+        mainframe = tk.Frame(self.master)
         mainframe.pack(pady=100)
-        mainframe.grid_rowconfigure(1, minsize=row_padding)
-        mainframe.grid_rowconfigure(2, minsize=row_padding)
-        mainframe.grid_rowconfigure(3, minsize=row_padding)
-        mainframe.grid_rowconfigure(4, minsize=row_padding)
 
         cat_var = tk.StringVar(self.master)
         cat_var.set(self.name_cat[0])
 
-        info = tk.Label(master=mainframe, text='Select Name Category!', fg='black')
-        info.grid()
+        info = tk.Label(self.master, text='Select Name Category!')
+        info.pack()
 
-        drop_down = tk.OptionMenu(mainframe, cat_var, *self.name_cat)
+        drop_down = ttk.OptionMenu(info, cat_var, *self.name_cat)
         drop_down.grid()
 
-        start_gen = tk.Button(master=mainframe, text='Generate Name & Surname',
-                              command=self.generate_names, width=btn_width)
+        start_gen = ttk.Button(info, text='Generate Name & Surname',
+                               command=self.generate_names, width=btn_width, style="MyButton")
         start_gen.grid()
 
-        start_gen = tk.Button(master=mainframe, text='Generate Name Only',
-                              command=self.generate_names, width=btn_width)
+        start_gen = ttk.Button(info, text='Generate Name Only',
+                               command=self.generate_names, width=btn_width)
         start_gen.grid()
 
-        start_gen = tk.Button(master=mainframe, text='Generate Surname Only',
-                              command=self.generate_names, width=btn_width)
+        start_gen = ttk.Button(info, text='Generate Surname Only',
+                               command=self.generate_names, width=btn_width)
         start_gen.grid()
 
-        close_app = tk.Button(master=mainframe, text='Quit',
-                              command=self.master.destroy, width=btn_width)
+        close_app = ttk.Button(info, text='Quit',
+                               command=self.master.destroy, width=btn_width)
         close_app.grid()
 
     def generate_names(self):
 
         pass
 
+    def configure_style(self):
 
-if __name__ == '__main__':
+        self.style = ttk.Style()
+
+        self.style.configure('MyFrame',
+                             background='#19232D')
+
+        self.style.configure('MyLabel',
+                             background='#19232D',
+                             foreground="#F0F0F0",
+                             font="Arial 20 bold")
+
+        self.style.configure('MyButton',
+                             background='#19232D',
+                             foreground="#F0F0F0",
+                             font="Arial 12")
+
+        self.style.configure('MyChechbox',
+                             background='#19232D',
+                             foreground="#F0F0F0",
+                             font="Arial 12")
+
+        self.style.configure('MyOption',
+                             background='#19232D',
+                             foreground="#F0F0F0",
+                             font="Arial 12")
+
+
+def main():
 
     root = tk.Tk()
-    root.geometry("500x500")
-    root.resizable(0, 0)
-    root.title("Name Generator")
 
-    app = NameGenerator(root)
+    _ = NameGenerator(root)
     root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
