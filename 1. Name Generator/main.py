2c47dc0bba35a7432a320bd15994797642d9f562
mrdominikku <dominik.ruczajewski@gmail.com>
2019-08-19 11:59:12 +0200

    added main loop to GUI tkinter

---
+++ 1. Name Generator/main.py
@@ -0,0 +1,16 @@
+import math
+import tkinter as tk
+
+
+class NameGenerator:
+
+    def __init__(self):
+
+        window = tk.Tk()
+        window.title("Name Generator")
+        window.mainloop()
+
+
+if __name__ == '__main__':
+
+    name_gen = NameGenerator()
