f096c9f218771bbe5ee7063f241209e076a64457
mrdominikku <dominik.ruczajewski@gmail.com>
2019-08-25 12:36:05 +0200

    added dependency with customized inherited TK widgets

--- 1. Name Generator/Python/config.py
+++ 1. Name Generator/Python/config.py
@@ -1,6 +1,9 @@
-config_colors = {
+config_layout = {
     'dark_blue': '#19232D',
-    'light_gray': '#F0F0F0'
+    'light_blue': '#505F69',
+    'light_gray': '#F0F0F0',
+    'hover_on': '#1464A0',
+    'pressed': '#32414B'
 }
 
 config_names = {
