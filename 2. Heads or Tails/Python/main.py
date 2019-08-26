a17d3afd9af5dbd100ff309afd0d65f6241a64a8
mrdominikku <dominik.ruczajewski@gmail.com>
2019-08-26 23:59:13 +0200

    disabled button to avoid multiprocessing set callback variable to break the canvas after loop using after_cancel for each flip coin set to default added image of performing app

--- 2. Heads or Tails/Python/main.py
+++ 2. Heads or Tails/Python/main.py
@@ -12,6 +12,7 @@ class NameGenerator(object):
         self.master.resizable(0, 0)
         self.master.title("Heads or Tails")
 
+        self.coin = None
         self.set_layout()
 
     def set_layout(self):
@@ -25,78 +26,91 @@ class NameGenerator(object):
         canvas = MyCanvas(coin_frame)
         canvas.pack(fill=BOTH)
 
-        coin = Coin(canvas, 265, 250, 35, 20)
-
         btn_frame = MyFrame(mainframe)
         btn_frame.pack(fill=BOTH)
 
         btn_flip = MyButton(btn_frame, text='Flip the Coin!', width=30)
-        btn_flip.configure(command=lambda button=btn_flip: self.flip_coin(coin))
+        btn_flip.configure(command=lambda button=btn_flip: self.flip_coin(btn_flip))
         btn_flip.pack(fill=BOTH)
 
         close_app = MyButton(btn_frame, text='Quit', command=self.master.destroy, width=30)
         close_app.pack(fill=BOTH)
 
-    def flip_coin(self, coin):
+        self.coin = Coin(canvas, btn_flip, 265, 250, 35, 20)
 
-        coin.repeat_count = 1
-        coin.repeat = random.randrange(5, 10)
-        coin.update_coin()
+    def flip_coin(self, btn_flip):
+
+        btn_flip.configure(state=DISABLED)
+
+        self.coin.canvas.coords(self.coin.coin,
+                                self.coin.default_x1, self.coin.default_y1, self.coin.default_x2, self.coin.default_y2)
+
+        self.coin.repeat_count = 1
+        self.coin.repeat = random.randrange(5, 10)
+        self.coin.update_coin()
 
 
 class Coin:
 
-    def __init__(self, canvas, x1, y1, x2, y2):
+    def __init__(self, canvas, btn_flip, x1, y1, x2, y2):
 
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
 
+        self.default_x1 = x1
+        self.default_y1 = y1
+        self.default_x2 = x2
+        self.default_y2 = y2
+
+        self.callback = None
+        self.btn_flip = btn_flip
+
         self.repeat = 5
         self.repeat_count = 1
-        self.interval = 5
+        self.interval = 10
         self.flip = ['HEADS', 'TAILS']
         self.flip_int = 1
-        self.width = 2
-        self.width_interval = 0.15
+        self.outline_width = 2
+        self.width_interval = 0.20
 
         self.fill = config_layout['pressed']
         self.outline = config_layout['light_gray']
 
         self.canvas = canvas
         self.coin = canvas.create_oval(self.x1, self.y1, self.x2, self.y2,
-                                       fill=self.fill, outline=self.outline, width=self.width)
+                                       fill=self.fill, outline=self.outline, width=self.outline_width)
 
         self.heads_tails = canvas.create_text((self.x1+self.x2)*0.5, (self.y1+self.y2)*0.5,
                                               text='Flip the Coin!', font='Times 25 bold')
 
     def update_coin(self):
 
-        self.x1, self.y1, self.x2, self.y2 = self.canvas.coords(self.coin)
+        self.x2, self.y2, self.x1, self.y1 = self.canvas.coords(self.coin)
 
-        if self.y1 >= 110:
+        if self.y2 > 120:
             self.interval *= -1
             self.flip_int = int(not self.flip_int)
             self.width_interval *= -1
             self.canvas.itemconfig(self.heads_tails, text=self.flip[self.flip_int])
 
-        if self.y1 <= 20:
+        if self.y2 < 20:
             self.width_interval *= -1
             self.interval *= -1
             self.repeat_count += 1
 
-            if self.repeat_count == self.repeat:
-                self.canvas.after_cancel(None)
-
-        self.y1 += self.interval
-        self.y2 -= self.interval
-        self.width += self.width_interval
+        self.y2 += self.interval
+        self.y1 -= self.interval
+        self.outline_width += self.width_interval
 
         self.canvas.coords(self.coin, self.x1, self.y1, self.x2, self.y2)
-        self.canvas.itemconfig(self.coin, width=self.width)
-        self.canvas.after(20, self.update_coin)
+        self.canvas.itemconfig(self.coin, width=self.outline_width)
+        self.callback = self.canvas.after(30, self.update_coin)
 
+        if self.repeat_count == self.repeat:
+            self.btn_flip.configure(state=NORMAL)
+            self.canvas.after_cancel(self.callback)
 
 
 def main():
